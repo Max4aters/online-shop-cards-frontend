07453e84b1e9aeeb9e332278f5de8078f5392fb5
Artem <44427870+arteemk@users.noreply.github.com>
2020-07-15 02:15:35 +0700

    major commit
    оптимизация js-кода и добавление семантики

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,18 @@
-// svg (- и +) помещены в кнопку, благодаря чему область клика увеличивается
-document.querySelectorAll('.options__quantity_changer').forEach(quantityChangerBtn => {
-    let signSvg = quantityChangerBtn.children[0];
-    quantityChangerBtn.onmousedown = () => {
-        signSvg.classList.toggle('options__quantity_sign_active');
-    }
-    quantityChangerBtn.onmouseup = () => {
-        signSvg.classList.toggle('options__quantity_sign_active');
-    }
-})
-
-let buyButtons = document.querySelectorAll('.options__buy-button');
-buyButtons.forEach(button => {
-    button.onmousedown = () => {
-        button.classList.toggle('options__buy-button_active');
-    }
-    button.onmouseup = () => {
-        button.classList.toggle('options__buy-button_active');
-    }
-})
-
-let minusButtons = document.getElementsByClassName('minus');
-for (let i = 0; i < minusButtons.length; i++) {
-    minusButtons[i].onclick = () => {
-        // на каждую кнопку "-" есть свое значение (товары для добавления в корзину). сопоставляем действие i-й кнопки i-му значению
-        let valueElement = document.getElementsByClassName('options__quantity_current-value')[i];
-        let numberToBuy = parseInt(valueElement.innerText);
-        // в корзину будет нельзя добавить количество товаров меньше 1
-        if (numberToBuy > 1) {
-            valueElement.innerText = (--numberToBuy).toString();
+document.querySelectorAll('.card__quantity_changer').forEach(button => {
+    button.onclick = () => {
+        let parent = button.parentElement; // <div class="card__quantity">
+        let span = parent.children[1]; // <span class="card__value-to-buy">
+        let input = span.children[0]; // <input type="text" name="product_quantity">
+        let sign = button.classList[1];
+        switch (sign) {
+            case 'minus':
+                if (input.value > 1) {
+                    --input.value;
+                }
+                break;
+            case 'plus':
+                ++input.value;
+                break;
         }
     }
-}
-let plusButtons = document.getElementsByClassName('plus');
-for (let i = 0; i < minusButtons.length; i++) {
-    plusButtons[i].onclick = () => {
-        let valueElement = document.getElementsByClassName('options__quantity_current-value')[i];
-        let numberToBuy = parseInt(valueElement.innerText);
-        valueElement.innerText = (++numberToBuy).toString();
-    }
-}
+})
